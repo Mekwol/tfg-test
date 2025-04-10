f78825fe5deb5192e4ab8643d321b6141f7de066
mekwol <mek9760@gmail.com>
2025-04-10 22:36:09 +0000

    delete both ec2s

--- ec2.tf
+++ ec2.tf
@@ -11,20 +11,20 @@ resource "aws_key_pair" "generated_key" {
   public_key = tls_private_key.ssh_key.public_key_openssh
 }
 # Create a bastion host in public subnet
-resource "aws_instance" "bastion" {
-  provider                    = aws.tfg-test-account1-region1
-  ami                         = "ami-0e1c5d8c23330dee3" # Amazon Linux 2023 AMI for us-east-1
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.region1_public_subnet1.id
-  vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
-  key_name                    = aws_key_pair.generated_key.key_name
-  associate_public_ip_address = true
+#resource "aws_instance" "bastion" {
+ # provider                    = aws.tfg-test-account1-region1
+  #ami                         = "ami-0e1c5d8c23330dee3" # Amazon Linux 2023 AMI for us-east-1
+  #instance_type               = "t2.micro"
+  #subnet_id                   = aws_subnet.region1_public_subnet1.id
+  #vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
+  #key_name                    = aws_key_pair.generated_key.key_name
+  #associate_public_ip_address = true
 
-  tags = {
-    Name        = "TFG-Test-Bastion"
-    Environment = "Test"
-  }
-}
+  #tags = {
+   # Name        = "TFG-Test-Bastion"
+    #Environment = "Test"
+  #}
+#}
 
 # Create a private instance in private subnet
 #resource "aws_instance" "private" {
