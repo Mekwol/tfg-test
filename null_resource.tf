719d4f7a0876fa8cc811e7324c9a6c7a8ddcbf26
mekwol <mek9760@gmail.com>
2025-04-11 18:04:13 +0000

    b

--- null_resource.tf
+++
@@ -1,9 +0,0 @@
-resource "null_resource" "prepare_token" {
-  triggers = {
-    always_run = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command = "bash ${path.module}/prepare-token.sh"
-  }
-}
