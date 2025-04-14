7554932fe149bd83cdc8d1c4afc349c364553a05
mekwol <mek9760@gmail.com>
2025-04-14 17:02:54 +0000

    testing changes

--- ipam_module.tf
+++ ipam_module.tf
@@ -1,12 +1,12 @@
 # ipam_module.tf (new file in root directory)
-module "ipam" {
-  source = "./modules/ipam"
+#module "ipam" {
+ # source = "./modules/ipam"
   
-  delegated_account_id = var.delegated_account_id
-  aws_regions          = var.aws_regions
-  tfg_test_account1_id = aws_organizations_account.tfg-test-account1.id
+  #delegated_account_id = var.delegated_account_id
+  #aws_regions          = var.aws_regions
+  #tfg_test_account1_id = aws_organizations_account.tfg-test-account1.id
   
-  providers = {
-    aws.delegated_account = aws.delegated_account
-  }
-}
+  #providers = {
+   # aws.delegated_account = aws.delegated_account
+  #}
+#}
