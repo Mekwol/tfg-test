98ab6ce82c5d86437fd33dd433b998e52574abf0
mekwol <mek9760@gmail.com>
2025-04-14 17:12:52 +0000

    update2

--- modules/security/variables.tf
+++ modules/security/variables.tf
@@ -7,3 +7,21 @@ variable "tfg_test_account1_id" {
   description = "ID of the test account"
   type        = string
 }
+
+variable "linux_ip" {
+  description = "The ip of my bastion host- linux insance"
+  type        = string
+}
+
+variable "my_ip" {
+  description = "my_ip"
+  type        = string
+i
+}
+
+variable "vpc_id_region1" {
+  description = "Region1 vpc id"
+  type        = string
+}
+
+
