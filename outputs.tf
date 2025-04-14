b68b15feada06563e27b4f226b9e4c493db8a966
mekwol <mek9760@gmail.com>
2025-04-14 13:37:22 +0000

    outputs file updated

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,20 @@
 # Output for easy reference
 output "bastion_public_ip" {
-  value = aws_instance.bastion.public_ip
+  description = "Public IP address of the bastion host"
+  value       = aws_instance.bastion.public_ip
 }
 
 output "private_instance_private_ip" {
-  value = aws_instance.private.private_ip
+  description = "Private IP address of the private instance"
+  value       = aws_instance.private.private_ip
 }
 
-# Add this to variables.tf
-variable "my_ip" {
-  description = "Your public IP address for SSH access"
-  type        = string
+output "region1_vpc_id" {
+  description = "ID of the VPC in region 1"
+  value       = aws_vpc.region1_vpc.id
+}
+
+output "region2_vpc_id" {
+  description = "ID of the VPC in region 2"
+  value       = aws_vpc.region2_vpc.id
 }
