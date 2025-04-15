1e98b856fb2663722e953fdb267dc34c878c3f03
mekwol <mek9760@gmail.com>
2025-04-15 16:14:28 +0000

    19

--- modules/ipam/variables.tf
+++
@@ -1,7 +0,0 @@
-# modules/ipam/variables.tf
-
-variable "aws_regions" {
-  type        = list(string)
-  description = "List of AWS regions to operate in"
-  default     = ["us-east-1", "us-east-2"]
-}
