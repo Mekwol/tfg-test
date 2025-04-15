48b690543110b6d91cbab6f2b5d3795923c28396
mekwol <mek9760@gmail.com>
2025-04-15 17:57:51 +0000

    28

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ terraform {
    required_providers {
     aws = {
       source  = "hashicorp/aws"
-     # version = "~> 5.20"
+      version = "~> 5.49.0"
     }
     time = {
       source  = "hashicorp/time"
