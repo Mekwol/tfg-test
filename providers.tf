3d84f598ebe1a9967c129c6408613b7a1c4ed4e5
mekwol <mek9760@gmail.com>
2025-04-15 13:20:57 +0000

    update

--- providers.tf
+++ providers.tf
@@ -24,6 +24,15 @@ provider "aws" {
 }
 
 provider "aws" {
+  alias  = "delegated_account"
+  region = var.aws_regions[1]
+  assume_role {
+    role_arn = "arn:aws:iam::${var.delegated_account_id}:role/OrganizationAccountAccessRole"
+  }
+}
+
+
+provider "aws" {
   alias  = "tfg-test-account1-region1"
   region = var.aws_regions[0]
   assume_role {
